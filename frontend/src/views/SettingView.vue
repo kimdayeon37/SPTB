38c017eb11b681ba63571faf26d22dfb5310641f
angrymusic <angrymusic@naver.com>
2023-12-27 17:39:40 +0900

    ip관리 setting 페이지 구현

--- frontend/src/views/SettingView.vue
+++ frontend/src/views/SettingView.vue
@@ -1,7 +1,82 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
+import IPList from '@/components/Setting/IP-List.vue'
+import { $axios } from '@/axios/index'
+
 const active = ref(false)
+const showInput = ref(false)
+const toggleInput = () => {
+  showInput.value = !showInput.value
+}
 const searchIp = ref('')
+const newIp = ref('')
+let ipList: string[] = []
+const filtedIpList = ref<string[]>([])
+const testIpList = ['125.32.123.1', '125.32.123.2', '125.32.123.3', '125.32.124.1']
+
+const sendToggleValue = async () => {
+  try {
+    const response = await $axios().post('/api/ipActive', {
+      active: active,
+    })
+    if (response.data.result) {
+      if (active.value) console.log('활성화 성공')
+      else console.log('비활성화 성공')
+    } else {
+      console.log('활성 상태 변경 실패')
+    }
+  } catch (error) {}
+}
+
+const deleteIp = async (ip: string) => {
+  try {
+    const response = await $axios().post('/api/deleteIp', { ip })
+    if (response.data.result) {
+      console.log('ip 삭제 성공')
+      await getIpList()
+    } else console.log('ip 삭제 실패')
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const addIp = async () => {
+  try {
+    const response = await $axios().post('/api/addIp', { ip: newIp.value })
+    if (response.data.result) {
+      console.log('ip 추가 성공')
+      await getIpList()
+    } else console.log('ip 추가 실패')
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const getIpList = async () => {
+  try {
+    const response = await $axios().get('/api/iplist')
+    active.value = response.data.active
+    filtedIpList.value = [...response.data.result]
+  } catch (error) {
+    console.error(error)
+    filtedIpList.value = [...testIpList]
+    ipList = [...testIpList]
+  }
+}
+
+watch(searchIp, () => {
+  if (searchIp.value === '') {
+    // searchIp가 비어있는 경우 모든 값을 담음
+    filtedIpList.value = [...ipList]
+  } else {
+    // searchIp로 시작하는 IP 주소만 필터링
+    filtedIpList.value = [...ipList.filter((ip) => ip.startsWith(searchIp.value))]
+  }
+})
+
+onMounted(async () => {
+  await getIpList()
+})
 </script>
 <template>
   <div>
@@ -9,9 +84,9 @@ const searchIp = ref('')
       <div>Setting > <strong>IP Blocking</strong></div>
     </div>
 
-    <div class="col-12 row q-ma-sm">
+    <div class="top col-12 row q-pa-sm">
       <div class="col-2 row items-center justify-center">
-        <q-toggle color="main" v-model="active" />
+        <q-toggle color="main" v-model="active" :onUpdate="() => sendToggleValue()" />
         <q-icon :color="active ? 'positive' : 'negative'" :name="active ? 'verified_user' : 'remove_moderator'" size="sm"></q-icon>
       </div>
 
@@ -19,29 +94,59 @@ const searchIp = ref('')
         <template v-slot:prepend>
           <q-icon name="search" />
         </template>
-        <template v-slot:append>
-          <q-btn flat label="검색" />
-        </template>
       </q-input>
 
-      <q-btn flat color="main" class="col-1"> 추가 </q-btn>
+      <q-btn flat color="main" class="col-1" @click="toggleInput" :icon="showInput ? 'expand_less' : 'add'"></q-btn>
     </div>
 
-    <q-list separator>
-      <q-item clickable v-ripple :active="active">
-        <q-item-section avatar>
+    <div class="row justify-end items-center" :class="showInput ? 'input-box input-box-after' : 'input-box'">
+      <q-input
+        class="col-9 noPadding"
+        outlined
+        dense
+        v-if="showInput"
+        v-model:model-value="newIp"
+        label="###.###.###.###"
+        :rules="[
+          (val) => !!val || '* Required',
+          (val) => {
+            const ipv4Pattern = /^(\d{1,3}\.){3}\d{1,3}$/ // IPv4 형식을 검증하는 정규 표현식
+            return ipv4Pattern.test(val) // 정규 표현식과 매치되는지 확인
+          },
+        ]"
+      >
+        <template v-slot:prepend>
           <q-icon name="public" />
-        </q-item-section>
-        <q-item-section>Active</q-item-section>
-        <q-item-section side><q-btn flat color="negative"> 삭제 </q-btn></q-item-section>
-      </q-item>
-      <q-item clickable v-ripple :active="active">
-        <q-item-section avatar>
-          <q-icon name="signal_wifi_off" />
-        </q-item-section>
-        <q-item-section>Active</q-item-section>
-        <q-item-section side>Side</q-item-section>
-      </q-item>
-    </q-list>
+        </template>
+      </q-input>
+      <q-btn flat dense color="main" class="col-1" @click="() => addIp()" icon="add"></q-btn>
+    </div>
+
+    <IPList v-model:active="active" v-model:searchIp="searchIp" v-model:filtedIpList="filtedIpList" @deleteIp="deleteIp" />
   </div>
 </template>
+<style lang="scss" scoped>
+.top {
+  border-bottom: solid 1px;
+  border-color: #bcbcbc;
+  background: #f3f4f5;
+}
+
+.input-box {
+  height: 0;
+  border-bottom: solid 1px;
+  border-color: #bcbcbc;
+  background: #f3f4f5;
+  transition-property: all;
+  transition-duration: 0.5s;
+  overflow: hidden;
+  font-size: 20px;
+}
+.input-box-after {
+  height: 56px !important;
+  padding: 7px;
+}
+.noPadding {
+  padding: 0 !important;
+}
+</style>
